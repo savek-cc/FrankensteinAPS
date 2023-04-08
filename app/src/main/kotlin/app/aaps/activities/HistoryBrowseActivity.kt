16ff2e8f204760733816b2a06575e4720c71ac12
Timm Korte <korte@korte-ing.de>
2023-04-08 15:02:32 +0200

    TK: Don't disable closed loop or maxiob because of missing objectives or old version TK: Allow to specify percentage of insulin required to be given as SMB instead of max/2 as a fixed limit TK: Allow zoom to 3h

--- app/src/main/kotlin/app/aaps/activities/HistoryBrowseActivity.kt
+++ app/src/main/kotlin/app/aaps/activities/HistoryBrowseActivity.kt
@@ -91,8 +91,8 @@ class HistoryBrowseActivity : TranslatedDaggerAppCompatActivity() {
             loadAll("onClickEnd")
         }
         binding.zoom.setOnClickListener {
-            var hours = rangeToDisplay + 6
-            hours = if (hours > 24) 6 else hours
+            var hours = if (rangeToDisplay == 3) rangeToDisplay + 3 else rangeToDisplay + 6
+            hours = if (hours > 24) 3 else hours
             rxBus.send(EventScale(hours))
         }
         binding.zoom.setOnLongClickListener {
