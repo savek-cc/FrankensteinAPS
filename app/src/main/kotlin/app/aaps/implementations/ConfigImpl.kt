7bf45332bf50855dd98c1c4a5eace82a001662af
Timm Korte <korte@korte-ing.de>
2025-07-30 18:09:33 +0200

    TK: Force engineering mode

--- app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
+++ app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
@@ -46,8 +46,8 @@ class ConfigImpl @Inject constructor(
     private var enableAutotune: Boolean? = null
     private var disableLeakCanary: Boolean? = null
 
-    override fun isEngineeringModeOrRelease(): Boolean = if (!APS) true else isEngineeringMode() || !isDev()
-    override fun isEngineeringMode(): Boolean = isEngineeringMode ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("engineering_mode") != null).also { isEngineeringMode = it }
+    override fun isEngineeringModeOrRelease(): Boolean = true
+    override fun isEngineeringMode(): Boolean = true
     override fun isUnfinishedMode(): Boolean = isUnfinishedMode ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("unfinished_mode") != null).also { isUnfinishedMode = it }
     override fun isDev(): Boolean = (VERSION.contains("-") || VERSION.matches(Regex(".*[a-zA-Z]+.*"))) && !VERSION.contains("-beta") && !VERSION.contains("-rc")
     override fun showUserActionsOnWatchOnly(): Boolean = showUserActionsOnWatchOnly ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("show_user_actions_on_watch_only") != null).also { showUserActionsOnWatchOnly = it }
