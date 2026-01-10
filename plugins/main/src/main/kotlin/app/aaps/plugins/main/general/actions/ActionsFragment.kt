2869f3b6ffdcec6ccd6b9a2d7293ac6dfcdd76fb
Timm Korte <korte@korte-ing.de>
2026-01-10 15:20:54 +0100

    TK: Get rid of warning popup when giving an extended Bolus

--- plugins/main/src/main/kotlin/app/aaps/plugins/main/general/actions/ActionsFragment.kt
+++ plugins/main/src/main/kotlin/app/aaps/plugins/main/general/actions/ActionsFragment.kt
@@ -117,12 +117,7 @@ class ActionsFragment : DaggerFragment() {
         binding.extendedBolus.setOnClickListener {
             activity?.let { activity ->
                 protectionCheck.queryProtection(activity, ProtectionCheck.Protection.BOLUS, UIRunnable {
-                    OKDialog.showConfirmation(
-                        activity, rh.gs(app.aaps.core.ui.R.string.extended_bolus), rh.gs(R.string.ebstopsloop),
-                        {
-                            uiInteraction.runExtendedBolusDialog(childFragmentManager)
-                        }, null
-                    )
+                    uiInteraction.runExtendedBolusDialog(childFragmentManager)
                 })
             }
         }
